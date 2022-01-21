a2c62d220d6fb3ae9d08f34f5a9809dc1eea3d58
Chris Fouts <cbfouts@micromat.com>
2022-01-21 14:25:49 -0800

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Christopher Fouts on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
